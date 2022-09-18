4c97cee583c87045700069c7e202b8d5f1360817
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-18 18:38:24 -0400

    Time: 331 ms (5.01%), Space: 19.9 MB (22.28%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,7 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        res = defaultdict(list)  
+        
+        #we are given that each str has a-z (26) values
+        #so we can maintain a count from [a-z] for each str in hashmap which will store like 1 e, 1 a, 1 t  
+        # key : 1 e, 1 a, 1 t
+        # value : lsit of strs having this values
+        
+        res = defaultdict(list)
         
         for s in strs:
             count = [0] * 26
@@ -11,4 +17,10 @@ class Solution:
                 
             res[tuple(count)].append(s)
             
-        return res.values()
+            
+        return res.values()                
+            
+                
+        
+        
+        

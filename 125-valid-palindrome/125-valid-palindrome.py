284246b532b755b9771ba18e980570982db87305
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-24 15:41:58 -0400

    Time: 100 ms (28.89%), Space: 14.5 MB (56.83%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,43 +1,25 @@
 class Solution:
-    
-            
-        
-
-    
     def isPalindrome(self, s: str) -> bool:
-        l,r=0,len(s)-1
         
-        while l<r:
-            while l<r and not self.alNum(s[l]):
-                l+=1
-            while r>l and not self.alNum(s[r]):
-                r-=1
-            if s[l].lower() != s[r].lower():
+        #using two pointers
+        i = 0  #start index
+        j = len(s) - 1   #end of string
+        
+        while i < j:    
+            
+            while i < j and not s[i].isalnum():   #if i < j and s[i] is a not alphanumeric character we increment i
+                i +=1
+                
+            while i < j and not s[j].isalnum():
+                j-=1
+                
+            if s[i].lower() != s[j].lower():
                 return False
             
-            l+=1
-            r-=1
+            
+            i +=1
+            j -=1
+            
             
         return True
-
-    def alNum(self,c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                    ord('a') <= ord(c) <= ord('z') or
-                    ord('0') <= ord(c) <= ord('9'))
-                
-
         
-        
-        
-        
-        
-
-        
-        
-#         newstr = ''
-#         for c in s:
-#             if c.isalnum():
-#                 newstr +=c.lower()
-                
-#         return newstr == newstr[::-1]
-

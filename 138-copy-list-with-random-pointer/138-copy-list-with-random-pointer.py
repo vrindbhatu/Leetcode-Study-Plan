ced2c8c2cac21a06f629872e666c14b98fe22378
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-23 23:28:09 -0400

    Time: 36 ms (95.65%), Space: 14.9 MB (46.60%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -10,22 +10,95 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
-        oldToCopy = {None : None}
-        
-        curr = head
-        
-        while curr:
-            copy = Node(curr.val)
-            oldToCopy[curr] = copy
-            curr = curr.next
+        oldToCopy = {None: None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
             
-        curr = head
-        
-        while curr:
-            copy = oldToCopy[curr]
-            copy.next = oldToCopy[curr.next]
-            copy.random = oldToCopy[curr.random]
-            curr = curr.next
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
             
         return oldToCopy[head]
+    
+    
+    
+    
+# class Node:
+#     def __init__(self,node):
+#         self.val = 0
+#         self.next = None
+#         self.random = None
+        
+# class Solution:
+#     def randomPointer(self, head):
+        
+#         hashMap = {None : None}
+        
+#         curr = head
+#         while curr:
+#             copy = Node(curr.val)
+#             hashMap[curr] = copy
+#             cuu = curr.next
+            
+#         curr = head
+#         while curr:
+#             copy = hashMap[curr]
+#             copy.next = hashMap[curr.next]
+#             copy.random = hashMap[curr.random]
+              # curr = curr.next
+            
+            
+#         return hashMap[head]
+            
+            
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         

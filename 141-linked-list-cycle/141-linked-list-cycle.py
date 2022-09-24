7ee1c191f609ab07c67cd86eca97571ac2365935
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-23 21:40:18 -0400

    Time: 119 ms (19.44%), Space: 17.9 MB (10.34%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,12 +6,43 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow , fast = head, head
-        
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
+        seen_nodes = set()
+        while head != None:
+            if head in seen_nodes:
                 return True
+            seen_nodes.add(head)
+            head = head.next
             
         return False
+
+    
+#time complexity : O(n) : we visit each of the n elements
+#space complexity: O(n) : n elements added to hashmap
+
+# Method 2
+
+# class ListNode:
+#     def __init__(self,node):
+#         self.val = node
+#         self.next = None
+        
+# class Solution:
+#     def hasCycle(self, head):
+#         slow = head
+#         fast = head.next
+        
+#         while slow!=fast:
+#             if fast = None or fast.next.next = None:
+#                 return False
+          
+#             slow = slow.next
+#             fast = flast.next.next
+            
+#         return True
+    
+    
+#time complexity : O(n) : we visit each of the n elements
+#space complexity: O(1) : we only use two nodes slow and fast
+        
+    
+        

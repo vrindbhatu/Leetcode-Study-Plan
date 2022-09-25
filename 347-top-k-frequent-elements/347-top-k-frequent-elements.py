c8456d1fe63b8cd433e21840cfe02e02ce5827e1
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-25 14:26:19 -0400

    Time: 194 ms (42.36%), Space: 19.6 MB (14.90%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,23 +1,50 @@
+#vrinda
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        hashMap = defaultdict(int)
+        hashMap = {}
+        freq = [[] for i in range(len(nums) +1)]
         
         for num in nums:
-            if num in hashMap:
-                hashMap[num] +=1
-            else:
-                hashMap[num] = 1
-                
-        newList = list(hashMap.items())
-        print(newList)
-        newList.sort(key = lambda x : x[1],reverse = True)
-        print(newList)
+            hashMap[num] = 1 + hashMap.get(num,0)
+            
+        for key, val in hashMap.items():
+            freq[val].append(key)
+            
         res = []
-        for key,value in newList[:k]:
-            res.append(key)
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+            if len(res) == k:
+                return res
             
-        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         hashMap = defaultdict(int)
+        
+#         for num in nums:
+#             if num in hashMap:
+#                 hashMap[num] +=1
+#             else:
+#                 hashMap[num] = 1
+                
+#         newList = list(hashMap.items())
+#         print(newList)
+#         newList.sort(key = lambda x : x[1],reverse = True)
+#         print(newList)
+#         res = []
+#         for key,value in newList[:k]:
+#             res.append(key)
             
-     
+#         return res
+            
+# O(nlogn)   
             

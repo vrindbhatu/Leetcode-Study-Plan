1573e96b8aba09ecd02578aa0ffa74139d5d3a57
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-25 12:23:19 -0400

    Time: 1157 ms (37.10%), Space: 0B (100.00%) - LeetHub

---
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+# Select seller_name as SELLER_NAME
+# from Seller s LEFT JOIN Orders o
+# ON s.seller_id = o.seller_id and YEAR(o.sale_date) = "2020"
+# WHere o.seller_id is NULL
+# order by seller_name
+
+
+select seller_name as SELLER_NAME
+from Seller
+where seller_id not in
+(select distinct(seller_id) from Orders where year(sale_date) = "2020")
+order by seller_name
+

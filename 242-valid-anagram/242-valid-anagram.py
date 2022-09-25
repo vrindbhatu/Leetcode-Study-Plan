b6a9091b55623cbb41d6766154f870f60ab7cd02
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-25 13:43:23 -0400

    Time: 54 ms (87.90%), Space: 14.5 MB (67.23%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,14 +1,18 @@
+
+
+#vrinda
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        s = sorted(s)
-        t = sorted(t)
         if len(s) != len(t):
             return False
         
-        for i in range(0,len(s)):
-            if s[i] !=t[i]:
-                return False
-
-        return True
+        hashMapS = {}
+        hashMapT = {}
         
+        for i in range(len(s)):
+            hashMapS[s[i]] = 1 + hashMapS.get(s[i],0)
+            hashMapT[t[i]] =  1 + hashMapT.get(t[i],0)
+            
+        return hashMapS == hashMapT
+    
+#time complexity O(s + t) as we need to iterate to both string and space complexity will we same

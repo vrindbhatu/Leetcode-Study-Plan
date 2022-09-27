f1337b848f990ae4b73c65329bfbb6c785baa053
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-09-26 20:05:50 -0400

    Time: 55 ms (39.54%), Space: 13.9 MB (75.58%) - LeetHub

---
+++ 100-same-tree/100-same-tree.py
@@ -0,0 +1,63 @@
+#vrinda : three condition if both null return true if one is null or values dont match return false
+# dfs: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left)
+    
+#bfs : 
+# class TreeNode:
+#     def __init__(self, val = 0,left = None,right = None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+        
+# class Solution:
+#     def isSameTree(self,p,q):
+        
+        
+        
+#         def isCheck(p,q):
+            
+#             if not p and not q:
+#                 return True
+#             if not p or not q:
+#                 return False
+#             if p.val != q.val:
+#                 return False
+#             return True
+        
+        
+#         queue = deque[(p,q),]
+        
+#         while queue:
+            
+#             p,q = queue.popleft()
+            
+#             if not isCheck(p,q):
+#                 return False
+            
+#             if p:
+#                 queue.append(p.left,q.left)
+#                 queue.append(p.right,q.right)
+                
+#             return True
+            
+
+        
+        
+        

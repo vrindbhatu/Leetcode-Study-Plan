72dea37dea87c35f842836e87cc8ae7fe8fa4fea
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-10-07 19:49:38 -0400

    Time: 409 ms (47.21%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,15 +1,5 @@
 # Write your MySQL query statement below
-WITH cte as
-(SELECT *
-FROM Sales
-ORDER BY sale_date),
-
-cte2 as
-(SELECT *, LEAD(sold_num,1) OVER(PARTITION BY sale_date) as orange_sold
-FROM cte)
-
-SELECT sale_date, (sold_num - orange_sold) as diff
-FROM  cte2
-WHERE orange_sold is not null
-ORDER BY sale_date
-    
+SELECT sale_date,  SUM(CASE WHEN fruit = "apples" then sold_num end) - SUM(CASE WHEN fruit = "oranges" then sold_num end) as diff
+from Sales
+group by sale_date
+order by sale_date

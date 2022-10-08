ef54029961e8a28463e0d33f1c413bb7d0e88289
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-10-07 23:05:55 -0400

    Time: 666 ms (21.42%), Space: 0B (100.00%) - LeetHub

--- 619-biggest-single-number/619-biggest-single-number.sql
+++ 619-biggest-single-number/619-biggest-single-number.sql
@@ -1,10 +1,19 @@
 # Write your MySQL query statement below
-select 
-case
-    when count(*) = 1 then num
-    else null
-end as num
+# select 
+# case
+#     when count(*) = 1 then num
+#     else null
+# end as num
+# from MyNumbers
+# group by num
+# order by num desc
+# limit 1
+
+
+
+select if(count(*) =1, num,null) as num
 from MyNumbers
 group by num
 order by num desc
 limit 1
+

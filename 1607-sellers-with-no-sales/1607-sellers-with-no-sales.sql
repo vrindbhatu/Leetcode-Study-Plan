b54e7dee377a5c39a0160fabd90638e205cd0f23
vrindbhatu <70003172+vrindbhatu@users.noreply.github.com>
2022-10-07 23:02:22 -0400

    Time: 820 ms (78.19%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -6,9 +6,15 @@
 # order by seller_name
 
 
+# select seller_name as SELLER_NAME
+# from Seller
+# where seller_id not in
+# (select distinct(seller_id) from Orders where year(sale_date) = "2020")
+# order by seller_name
+
+
+
 select seller_name as SELLER_NAME
 from Seller
-where seller_id not in
-(select distinct(seller_id) from Orders where year(sale_date) = "2020")
+where seller_id not in (select seller_id from Orders where Year(sale_date) = 2020)
 order by seller_name
-
